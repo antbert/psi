5b4b3cf27cfb231675c8e6ecef2db392bff39e9e
alehatsman <olegruno@gmail.com>
2015-02-24 17:07:53 +0300

    Project structure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,62 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        postcss: {
-            options: {
-                processors: [
-                    require('autoprefixer-core')({
-                        browsers: ['last 2 version', 'ie 8', 'ie 9']
-                    }).postcss,
-                    require('csswring').postcss
-                ]
-            },
-            dist: {
-                src: 'public/stylesheets/*.css'
-            }
-        },
+  grunt.initConfig({
+      postcss: {
+          options: {
+              processors: [
+                  require('autoprefixer-core')({
+                      browsers: ['last 2 version', 'ie 8', 'ie 9']
+                  }).postcss,
+                  require('csswring').postcss
+              ]
+          },
+          dist: {
+              src: 'public/stylesheets/*.css'
+          }
+      },
 
-        sass: {
-            dist: {
-                options: {
-                    sourcemap: 'none'
-                },
-                files: {
-                    'public/stylesheets/style.css': 'assets/sass/style.scss'
-                }
-            }
-        },
+      sass: {
+          dist: {
+              options: {
+                  sourcemap: 'auto'
+              },
+              files: {
+                  'public/stylesheets/style.css': 'public/assets/sass/style.scss'
+              }
+          }
+      },
 
-        watch: {
-            postcss: {
-                files: ['**/*.css'],
-                tasks: ['postcss']
-            },
-            sass: {
-                files: ['**/*.{scss,sass}'],
-                tasks: ['sass']
+      browserify: {
+        main: {
+          options: {
+            browserifyOptions: {
+              debug: true
             }
+          },
+          src: 'public/assets/js/**.js',
+          dest: 'public/js/index.js'
         }
-    });
+      },
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-postcss');
+      watch: {
+          postcss: {
+              files: ['**/*.css'],
+              tasks: ['postcss']
+          },
+          sass: {
+              files: ['**/*.{scss,sass}'],
+              tasks: ['sass']
+          }
+      }
+  });
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['sass', 'postcss']);
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['sass', 'postcss', 'browserify']);
 
 };
 
