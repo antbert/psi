fbbc3a6f6725b3063cb38f688b0bd849c6a0f909
Aleh Atsman <aleh_atsman@epam.com>
2015-02-24 18:15:33 +0300

    Test coverage.

--- tests/hello-test.js
+++ tests/hello-test.js
@@ -1,7 +1,9 @@
 var expect = require('chai').expect;
+var s = require('../app/service/helloService');
 
 describe('Hello world bdd with mocha and chai', function() {
   it('Expected that it should be fub', function() {
     expect('Fun').to.equal('Fun');
+    expect(s(1, 2)).to.equal(3);
   });
 });
