0d7f6d47ccb1c7adbb9e3881ea945810c32e04ac
alehatsman <olegruno@gmail.com>
2015-02-24 23:39:16 +0300

    Reformat code.

--- tests/hello-test.js
+++ tests/hello-test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var s = require('../app/service/helloService');
 
 describe('Hello world bdd with mocha and chai', function() {
-  it('Expected that it should be fub', function() {
+  it('Expected that it should be fun', function() {
     expect('Fun').to.equal('Fun');
     expect(s(1, 2)).to.equal(3);
   });
