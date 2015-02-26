f2431ac1f8897821b7a9b2361754686feedfb888
alehatsman <olegruno@gmail.com>
2015-02-26 10:31:34 +0300

    HJS tests.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { name: 'Express' }, function(err, html) {
+    res.render('template', {content: html}, function(err, html) {
+      res.send(html);
+    });
+  });
 });
 
 module.exports = router;
