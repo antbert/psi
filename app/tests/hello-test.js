7e19443f702806a4fc15013f88b90257e1a62650
alehatsman <olegruno@gmail.com>
2015-03-04 00:38:26 +0300

    Added tests.

--- app/tests/hello-test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var s = require('../app/service/helloService');
-
-describe('Hello world bdd with mocha and chai', function helloWorld() {
-  it('Expected that it should be fun', function fun() {
-    expect('Fun').to.equal('Fun');
-    expect(s(1, 2)).to.equal(3);
-  });
-});
