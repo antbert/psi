1f755132937813f41fa28e260c8944b1b490fbef
alehatsman <olegruno@gmail.com>
2015-03-27 19:09:29 +0300

    rtc local video bug.

--- app/packages/psd-rtc/lib/client/templates/rtc.js
+++ app/packages/psd-rtc/lib/client/templates/rtc.js
@@ -3,7 +3,7 @@
  */
 
 var config = {'iceServers': [
-		    {url:'stun:stun01.sipphone.com'},
+			{url:'stun:stun01.sipphone.com'},
 			{url:'stun:stun.ekiga.net'},
 			{url:'stun:stun.fwdnet.net'},
 			{url:'stun:stun.ideasip.com'},
@@ -48,17 +48,23 @@ Template.rtc.events({
 	'click .chat-btn': function(event, template) {
 		var remoteId = template.find('.input-message').value;
 		navigator.getUserMedia(c, function(stream) {
-			localPeer.call(remoteId, stream);
+			var local = document.getElementById('local-video');
+			local.src = URL.createObjectURL(stream);
+			var call = localPeer.call(remoteId, stream);
+			call.on('stream', function(remoteStream) {
+				var video = document.getElementById('remote-video');
+				video.src = URL.createObjectURL(remoteStream);
+			});
 		}, function(err) {
 			console.log(err);
 		});
-		
+
 	}
 });
 
 Meteor.startup(function () {
 	localPeer = new Peer({
-		host: 'pseudo-interview-signal.herokuapp.com', 
+		host: 'pseudo-interview-signal.herokuapp.com',
 		port: 80,
 		config: config
 	});
@@ -68,20 +74,16 @@ Meteor.startup(function () {
 	});
 
 	localPeer.on('call', function(call) {
-	  console.log(call);
-	  navigator.getUserMedia(c, function(s) {
-	  	var local = document.getElementById('local-video');
-	  	local.src = URL.createObjectURL(s);
-	  	call.answer(s);
-	  }, function(e) {
-	  	console.log(e);
-	  });
-
-	  call.on('stream', function(stream) {
-	  	console.log(stream);
-		var video = document.getElementById('remote-video');
-		video.src = URL.createObjectURL(stream);
-	  });
+		navigator.getUserMedia(c, function(s) {
+			var local = document.getElementById('local-video');
+			local.src = URL.createObjectURL(s);
+			call.answer(s);
+		}, function(e) {
+			console.log(e);
+		});
+		call.on('stream', function(stream) {
+			var video = document.getElementById('remote-video');
+			video.src = URL.createObjectURL(stream);
+		});
 	});
-
 });
