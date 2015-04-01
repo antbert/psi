f0c00ee8568518c63af43a624dee4bb6051674e3
Aleh Atsman <aleh_atsman@epam.com>
2015-04-01 23:16:31 +0300

    Python e2e tests. Code quality.

--- tests/python/accounts/testLanding.py
+++ tests/python/accounts/testLanding.py
@@ -2,17 +2,18 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PSDLanding(unittest.TestCase):
+class PSDLandingTestCase(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path="/Applications/chromedriver")
 
-    def testTitle(self):
+    def test_LoginLinkShouldGoToLoginForm(self):
         driver = self.driver
         driver.get("http://localhost:3000")
         assert "Pseudo-interview" in driver.title
+        loginLink = driver.find_element_by_class_name("login-link")
+        loginLink.click()
+        assert "/login" in driver.current_url
 
     def tearDown(self):
         self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
---
+++ tests/readme.txt
@@ -0,0 +1,3 @@
+To run all tests run:
+"python -m unittest discover"
+in console
