0b88978fbdee0c6fa0173f0676a2f05b8414cde5
Aleh Atsman <aleh_atsman@epam.com>
2015-04-06 00:15:44 +0300

    Refactored gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
-var SASS = './app/client/sass';
-gulp.task('sass2', function () {
-  return sass(SASS, { sourcemap: true })
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-    })
-    .pipe(sourcemaps.write('maps'))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./app/client/styles'));
-});
+var JS_FILES = [
+  'gulpfile.js',
+  'app/client/**/*.js',
+  'app/server/**/*.js',
+  'app/packages/**/*.js'
+];
 
 gulp.task('sass', function() {
   gulp.src('./app/client/sass/*.scss')
@@ -29,24 +24,14 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./app/client/styles'));
 });
 
-var jshint = require('gulp-jshint');
-
-var JS_FILES = [
-  'gulpfile.js',
-  'app/client/**/*.js',
-  'app/server/**/*.js',
-  'app/packages/**/*.js'
-];
-
 gulp.task('jshint', function () {
-  return gulp.src(JS_FILES)
+  gulp.src(JS_FILES)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-var jscs = require('gulp-jscs');
 gulp.task('jscs', function () {
-  return gulp.src(JS_FILES)
+  gulp.src(JS_FILES)
     .pipe(jscs({esnext: true}));
 });
 
