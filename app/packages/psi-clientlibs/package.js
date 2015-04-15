8c86eb8916088a52a5e1824e2cc1992343f400c8
Aleh Atsman <aleh_atsman@epam.com>
2015-04-15 23:36:59 +0300

    Added html5shiv.

--- app/packages/psi-clientlibs/package.js
+++ app/packages/psi-clientlibs/package.js
@@ -8,10 +8,12 @@ Package.describe({
 
 Package.onUse(function onUse(api) {
   api.addFiles([
-    '.npm/package/node_modules/normalize.css/normalize.css'
+    '.npm/package/node_modules/normalize.css/normalize.css',
+    '.npm/package/node_modules/html5shiv/dist/html5shiv.js'
   ], 'client');
 });
 
 Npm.depends({
-  'normalize.css': '3.0.2'
+  'normalize.css': '3.0.2',
+  'html5shiv': '3.7.2'
 });
